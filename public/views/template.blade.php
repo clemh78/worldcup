761ace47e022b6be172447e53efa20275018b588
Clément Hémidy <clemh78@gmail.com>
2018-06-08 20:02:12 +0200

    ajout nouvelle police

--- public/views/template.blade.php
+++ public/views/template.blade.php
@@ -35,6 +35,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=El+Messiri:400,500,600,700" rel="stylesheet">
 
     @yield('scripts')
 
